19e2a198ee00f1e86463193cb99bc55d45c8ca44
mahakgupta2729 <147686169+mahakgupta2729@users.noreply.github.com>
2023-10-12 10:35:14 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # mahakgupta2729
    # Input:
    # p, principal amount
    # t, time period in years
